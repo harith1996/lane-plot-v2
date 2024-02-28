c4a314c591516bcd0c0deb24ee329335bdb4f471
Harith Rathish <harith1996@gmail.com>
2024-02-28 17:16:51 +0100

    added web scraper for articles

---
+++ backend/data_fetching/wikipedia/page_scraper.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Get the most revised pages on Wikipedia
+def get_most_edited_pages(limit=250):
+    response = requests.get(
+        url="https://en.wikipedia.org/w/index.php?title=Special:MostRevisions&limit="+str(limit)+"&offset=0",
+    )
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    #get all ordered list with class "special"
+    ordered_lists = soup.find_all('ol', class_="special")
+
+    #find all links in the ordered list
+    page_names = []
+    for ordered_list in ordered_lists:
+        for link in ordered_list.find_all('a'):
+            if(link.get('href').startswith('/w/index.php?title=')):
+                page_names.append(link.get('title'))
+    return page_names
