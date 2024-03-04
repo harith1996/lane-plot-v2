fff23d3858097ec1c510a6a80f2ee1f8778d9f27
Harith Rathish <harith1996@gmail.com>
2024-03-04 11:56:37 +0100

    added filterTypes

---
+++ src/types/FilterTypes.ts
@@ -0,0 +1,15 @@
+export interface LaNePlotFilters  {
+    linearizeBy: string;
+    eventType: string;
+    sliceBy: string;
+    sliceByValue: string;
+    shownPlots: string[];
+}
+
+export interface LaNePlotFilterOptions  {
+    linearizeBy: string[];
+    eventType: string[];
+    sliceBy: string[];
+    sliceByValue: string[];
+    shownPlots: string[];
+}
